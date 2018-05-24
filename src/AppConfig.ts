a2eaebea4802e2a318155596597659b293d1c1e7
William Weiss <william@codification.org>
2018-05-24 18:34:43 -0500

    Some work on deployment configuration.

--- src/AppConfig.ts
+++ src/AppConfig.ts
@@ -1,9 +1,6 @@
 export class AppConfig {
-  public readonly s3Bucket = process.env['S3_BUCKET'];
-  private _eventKey = AppConfig.valueOrNull('EVENT_KEY');
-  private eventKeyPrefix = AppConfig.valueOrNull('EVENT_KEY_PREFIX');
-  private eventKeySuffix = AppConfig.valueOrNull('EVENT_KEY_SUFFIX');
-  private _eventContentType = AppConfig.valueOrNull('EVENT_CONTENT_TYPE');
+  public readonly distributionId = process.env['DISTRIBUTION_ID'];
+  private _invalidationPaths = AppConfig.valueOrNull('INVALIDATION_PATHS');
 
   static valueOrNull(key: string): string {
     let rval = process.env[key];
@@ -13,21 +10,11 @@ export class AppConfig {
     return rval;
   }
 
-  get eventKey(): string {
-    let rval = this._eventKey;
-    if (!rval) {
-      const suffix = this.eventKeySuffix ? this.eventKeySuffix : '.json';
-      rval = `${this.eventKeyPrefix}event-${Math.round(
-        Date.now() / 1000
-      )}${suffix}`;
-    }
-    return rval;
-  }
-
-  get eventContentType(): string {
-    let rval = this._eventContentType;
-    if (!rval && !this._eventKey && !this.eventKeySuffix) {
-      rval = 'application/json';
+  get invalidationPaths(): string[] {
+    let rval = [];
+    let raw = this._invalidationPaths;
+    if (raw) {
+      rval = raw.split(',');
     }
     return rval;
   }
